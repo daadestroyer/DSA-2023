2fd1c4fd9d74a11f499e4e58a55319912f9eb5fe
Shubham Nigam <apple@Apples-MacBook-Pro.local>
2023-05-22 14:35:11 +0530

    fruits into basket leetcode question addedd..

--- src/main/java/com/slidingwindow_twopointer/App3_FruitIntoBaskets.java
+++ src/main/java/com/slidingwindow_twopointer/App3_FruitIntoBaskets.java
@@ -5,8 +5,7 @@ import java.util.Map;
 
 public class App3_FruitIntoBaskets {
     public static void main(String[] args) {
-//        int[] fruits = {0, 1, 2, 2};
-        int[] fruits = {1, 2, 1};
+        int[] fruits = {3, 3, 3, 1, 2, 1, 1, 2, 3, 3, 4};
         Map<Integer, Integer> hm = new HashMap<>();
 
         int left = 0, right = 0;
@@ -16,7 +15,10 @@ public class App3_FruitIntoBaskets {
         while (right < fruits.length) {
             hm.put(fruits[right], hm.getOrDefault(fruits[right], 0) + 1);
             while (hm.size() > 2) {
-                hm.remove(fruits[left]);
+                hm.put(fruits[left], hm.get(fruits[left]) - 1);
+                if (hm.get(fruits[left]) == 0) {
+                    hm.remove(fruits[left]);
+                }
                 left++;
             }
             res = Math.max(res, right - left + 1);
