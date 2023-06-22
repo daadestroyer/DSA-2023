1a69bbd2d77ec375400acc41541afd4dde6108e9
Shubham Nigam <apple@Apples-MacBook-Pro.local>
2023-06-22 12:12:46 +0530

    reverse a linkedlist completed...

---
+++ src/main/java/com/linkedlist/programs/App1_ReverseALinkedList.java
@@ -0,0 +1,23 @@
+package com.linkedlist.programs;
+
+public class App1_ReverseALinkedList {
+    public static void main(String[] args) {
+        /*
+        question : 1 -> 2 -> 3 -> 4 -> 5
+        answer:    5 -> 4 -> 3 -> 2 -> 1
+
+        public ListNode reverseList(Node head) {
+            Node newHead = null;
+            Node temp = null;
+            while(head != null){
+                temp = head.next;
+                head.next = newHead;
+                newHead = head;
+                head = next;
+            }
+
+        }
+
+        */
+    }
+}
