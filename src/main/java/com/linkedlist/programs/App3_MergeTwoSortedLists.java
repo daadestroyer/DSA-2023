0ccc33090fe28874035164b9aa4b3c7aec1c9e28
Shubham Nigam <apple@Apples-MacBook-Pro.local>
2023-06-23 13:46:15 +0530

    merge two sorted list completed...

---
+++ src/main/java/com/linkedlist/programs/App3_MergeTwoSortedLists.java
@@ -0,0 +1,44 @@
+package com.linkedlist.programs;
+
+public class App3_MergeTwoSortedLists {
+    public static void main(String[] args) {
+        /*
+            public Node mergeTwoList(Node l1 , Node l2){
+                if(l1 == null)return l2;
+                if(l2 == null)return l1;
+
+                Node newHead = null;
+                Node temp = null;
+
+                if(l1.val <= l2.val){
+                    temp = l1;
+                    l1 = l1.next;
+                }
+                else{
+                    temp = l2;
+                    l2 = l2.next;
+                }
+                newHead = temp
+
+                while(l1 != null && l2 != null){
+                    if(l1.val <= l2.val){
+                        temp.next = l1;
+                        l1 = l1.next;
+                    }
+                    else{
+                        temp.next = l2
+                        l2 = l2.next;
+                    }
+                }
+
+                if(l1 == null){
+                    temp.next = l2;
+                }
+                else{
+                    temp.next = l1;
+                }
+            }
+            return newHead;
+        */
+    }
+}
