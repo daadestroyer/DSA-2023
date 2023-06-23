96108cd65a0385977b2e6c90137b2bd2e5a396ba
Shubham Nigam <apple@Apples-MacBook-Pro.local>
2023-06-23 10:51:13 +0530

    find middle of linkedlist completed...

--- src/main/java/com/linkedlist/programs/App2_FindMiddleOfTheLinkedList.java
+++ src/main/java/com/linkedlist/programs/App2_FindMiddleOfTheLinkedList.java
@@ -3,6 +3,16 @@ package com.linkedlist.programs;
 public class App2_FindMiddleOfTheLinkedList {
     public static void main(String[] args) {
         /*
+            1 -> 2 -> 3 -> 4 -> 5 -> null
+
+            Node slow = head;
+            Node fast = head;
+
+            while(fast != null && fast.next != null){
+                slow = slow.next;
+                fast = fast.next.next;
+            }
+            return slow;
 
          */
     }
