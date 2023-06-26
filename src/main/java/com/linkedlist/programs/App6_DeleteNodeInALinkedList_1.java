2c99cf6b48a5555e36faf231f68d4851febf7dbe
Shubham Nigam <apple@Apples-MacBook-Pro.local>
2023-06-26 11:59:24 +0530

    delete node in linkedlist - 1 added.

---
+++ src/main/java/com/linkedlist/programs/App6_DeleteNodeInALinkedList_1.java
@@ -0,0 +1,19 @@
+package com.linkedlist.programs;
+
+public class App6_DeleteNodeInALinkedList_1 {
+    public static void main(String[] args) {
+        /*
+           4 -> 5 -> 1 -> 9 -> X
+           4 -> 5 -> 9 -> X
+
+           public void deleteNode(Node node) {
+                // 4 -> 1 -> 1 -> 9
+                node.val = node.next.val;
+
+               // 4 -> 1 -> 9
+               node.next = node.next.next;
+           }
+
+        */
+    }
+}
