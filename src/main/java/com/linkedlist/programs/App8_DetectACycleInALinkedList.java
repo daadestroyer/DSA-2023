abb1dc441eecf9a3c42471d0216b1a6c3e4dcef7
Shubham Nigam <apple@Apples-MacBook-Pro.local>
2023-06-26 13:03:10 +0530

    detect loop in a linkedlist...

---
+++ src/main/java/com/linkedlist/programs/App8_DetectACycleInALinkedList.java
@@ -0,0 +1,24 @@
+package com.linkedlist.programs;
+
+public class App8_DetectACycleInALinkedList {
+    public static void main(String[] args) {
+        /*
+             3 -> 2 -> 0 -> -4 -> 2
+
+             public boolean hasCycle(Node head) {
+                Node slow = head;
+                Node fast = head;
+
+                while(fast != null || fast.next != null){
+                    slow = slow.next;
+                    fast = fast.next.next;
+
+                    if(slow == fast){
+                        return true
+                    }
+                }
+                return false;
+             }
+        */
+    }
+}
