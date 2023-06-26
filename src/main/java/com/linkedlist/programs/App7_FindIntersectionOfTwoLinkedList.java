bec0c64061f4abd6aa528677fa69ad7849f6bec3
Shubham Nigam <apple@Apples-MacBook-Pro.local>
2023-06-26 12:09:11 +0530

    find intersection of two linkedlist completed...

---
+++ src/main/java/com/linkedlist/programs/App7_FindIntersectionOfTwoLinkedList.java
@@ -0,0 +1,22 @@
+package com.linkedlist.programs;
+
+public class App7_FindIntersectionOfTwoLinkedList {
+    public static void main(String[] args) {
+        /*
+            A:       a1 -> a2
+                               -> c1 -> c2 -> c3 -> X
+            B: b1 -> b2 -> b3
+
+            public Node getIntersectionNode(Node headA, Node headB) {
+
+                Node left = headA;
+                Node right = headB;
+
+                while(left != right){
+                    left = left == null ? headB : left.next;
+                    right = right == null ? headA : right.next;
+                }
+            }
+        */
+    }
+}
