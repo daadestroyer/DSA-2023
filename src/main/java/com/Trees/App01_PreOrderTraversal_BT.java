320a6e80bf7505fc80af4bf14109792968e4c1df
Shubham Nigam <48306820+daadestroyer@users.noreply.github.com>
2023-11-21 21:38:03 +0530

    Trees (preOrderTraversal
    144. BT preOrderTraversal Completed

---
+++ src/main/java/com/Trees/App01_PreOrderTraversal_BT.java
@@ -0,0 +1,39 @@
+package com.Trees;
+
+import java.awt.image.AreaAveragingScaleFilter;
+import java.sql.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+
+class Node{
+     int data;
+     Node left;
+     Node right;
+     public Node(int data){
+         this.data = data;
+     }
+}
+public class App01_PreOrderTraversal_BT {
+
+    public static void preorderTraversal(Node root,List<Integer> list) {
+        if(root == null)
+            return ;
+
+        list.add(root.data);
+        preorderTraversal(root.left,list);
+        preorderTraversal(root.right,list);
+
+    }
+
+    public static void main(String[] args) {
+        List<Integer> list = new ArrayList<>();
+        preorderTraversal(new Node(10),list);
+        preorderTraversal(new Node(11),list);
+        preorderTraversal(new Node(13),list);
+        preorderTraversal(new Node(3),list);
+        preorderTraversal(new Node(4),list);
+        System.out.println(list);
+    }
+}
---
+++ src/main/java/com/stack/App11_CelebrityProblem.java
@@ -0,0 +1,76 @@
+package com.stack;
+
+import java.util.*;
+
+public class App11_CelebrityProblem {
+
+    static int findCelebrity(int[][] arr) {
+        Stack<Integer> st = new Stack<>();
+        for (int i = 0; i < arr.length; i++) {
+            st.push(i);
+        }
+
+        while (st.size() >= 2) {
+            int i = st.pop();
+            int j = st.pop();
+
+            if (arr[i][j] == 1) {
+                // if i knows j -> i is not celebrity
+                st.push(j);
+            } else {
+                // if i dont know j means i is celebrity
+                st.push(i);
+            }
+        }
+        int pot = st.pop();
+        for (int i = 0; i < arr.length; i++) {
+            if (i != pot) {
+                if (arr[i][pot] == 0 || arr[pot][i] == 1) {
+                    return -1;
+                }
+            }
+        }
+        return pot;
+    }
+
+    public static int findCelebrity1(int[][] arr) {
+        Stack<Integer> st = new Stack<>();
+        int celebrity = 0;
+        for (int i = 0; i < arr.length; i++) {
+            st.push(i);
+        }
+
+        while (st.size() >= 2) {
+            int i = st.pop();
+            int j = st.pop();
+
+            if (arr[i][j] == 1) {
+                st.push(j);
+            } else {
+                st.push(i);
+            }
+        }
+        celebrity = st.pop();
+        for (int i = 0; i < arr.length; i++) {
+            if (i != celebrity) {
+                if (arr[i][celebrity] == 0 && arr[celebrity][i] == 1) {
+                    return -1;
+                }
+            }
+        }
+        return celebrity;
+    }
+
+    public static void main(String[] args) {
+
+        int[][] arr = {
+                {0, 1, 1, 0},
+                {1, 0, 1, 0},
+                {0, 0, 0, 0},
+                {1, 1, 1, 0}
+        };
+
+        System.out.println(findCelebrity1(arr));
+
+    }
+}
