d3c315c623730376c6df35be46792e8bab3ce06c
Shubham Nigam <48306820+daadestroyer@users.noreply.github.com>
2023-11-22 17:11:52 +0530

    Create App02_InOrderTraversal_BT.java
    LevelOrderTraversal completed...

---
+++ src/main/java/com/Trees/App02_InOrderTraversal_BT.java
@@ -0,0 +1,38 @@
+package com.Trees;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+public class App02_InOrderTraversal_BT {
+
+/*
+    public List<List<Integer>> levelOrderTraversal(Node root){
+        Queue<Integer> q = new LinkedList<>();
+        List<List<Integer> list = new ArrayList<>();
+
+        if(root == null)
+            return list;
+
+        q.add(root);
+        while(!q.isEmpty()){
+            int size = q.size();
+            for(int i=0 ; i<size ; i++){
+                if(q.peek().left != null)
+                    q.add(q.peek().left)
+                if(q.peek().right != null)
+                    q.add(q.peek().right)
+
+                subList.add(q.remove().val)
+            }
+        }
+        return list;
+    }
+
+*/
+    public static void main(String[] args) {
+        List<List<Integer>> al = new ArrayList<>();
+        Queue<Integer> q = new LinkedList<>();
+    }
+}
