ec0306bc02b02a7f5c1bf75c1e6ce190170918d9
Shubham Nigam <48306820+daadestroyer@users.noreply.github.com>
2023-11-24 13:29:45 +0530

    Create App04_CheckBinaryTreeIsBalance_BTOrNot.java
    Check binary tree is balance binary tree or not

---
+++ src/main/java/com/Trees/App04_CheckBinaryTreeIsBalance_BTOrNot.java
@@ -0,0 +1,26 @@
+package com.Trees;
+
+public class App04_CheckBinaryTreeIsBalance_BTOrNot {
+
+/*
+    public int dfsHeight(TreeNode root){
+        if(root == null){
+            return 0;
+        }
+        int leftHeight = dfsHeight(root.left);
+        if(leftHeight == -1){
+            return -1;
+        }
+        int rightHeight = dfsHeight(root.right);
+        if(rightHeight == -1){
+            return -1;
+        }
+        if(Math.abs(leftHeight - rightHeight) > 1){
+            return -1;
+        }
+    }
+ */
+    public static void main(String[] args) {
+
+    }
+}
