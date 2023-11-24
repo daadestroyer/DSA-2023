e3ef273c31ee9e67f6c8bb62ea5b07e3d7a61221
Shubham Nigam <48306820+daadestroyer@users.noreply.github.com>
2023-11-24 22:16:41 +0530

    Create App05_DiameterOF_BT.java
    diameter of binary tree completed...

---
+++ src/main/java/com/Trees/App05_DiameterOF_BT.java
@@ -0,0 +1,25 @@
+package com.Trees;
+
+public class App05_DiameterOF_BT {
+/*
+    int diameter = 0;
+    public int maxDepth(TreeNode root){
+        if(root == null) return 0;
+
+        int leftH = maxDepth(root.left);
+        int rightH = maxDepth(root.right);
+
+        // calculate diameter of binary tree (leftH + rightH)
+        diameter = Math.max(diameter,leftH + rightH);
+
+        // sending height of current node from both left and right side
+        return Math.max(left , right) + 1;
+
+
+    }
+
+*/
+    public static void main(String[] args) {
+
+    }
+}
