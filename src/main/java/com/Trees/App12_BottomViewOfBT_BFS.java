7211f438b1f089231399292e501d7eba3865e467
Shubham Nigam <48306820+daadestroyer@users.noreply.github.com>
2023-12-20 17:03:12 +0530

    coded added..

---
+++ src/main/java/com/Trees/App12_BottomViewOfBT_BFS.java
@@ -0,0 +1,52 @@
+package com.Trees;
+
+import java.util.Map;
+import java.util.TreeMap;
+
+/*
+class Pair{
+    Node node;
+    int verticalIndex;
+
+    Pair(int verticalIndex,Node node){
+        this.verticalIndex = verticalIndex;
+        this.node = node;
+    }
+}
+*/
+public class App12_BottomViewOfBT_BFS {
+
+    /*
+          public ArrayList <Integer> bottomView(Node root){
+              ArrayList<Integer> al = new ArrayList<>();
+              Queue<Pair> q = new LinkedList<>();
+              Map<Integer,Integer> hm = new TreeMap<>();
+              q.add(new Pair(0,root));
+
+              while(!q.isEmpty()){
+                    Pair p = q.remove();
+                    int hDist = p.hDist;
+                    Node tempNode = p.node;
+
+                    hm.put(hDist,tempNode.data);
+
+                    if(tempNode.left != null){
+                        q.add(new Pair(hDist-1,tempNode.left));
+                    }
+                    if(tempNode.right != null){
+                        q.add(new Pair(hDist+1,tempNode.right));
+                    }
+              }
+              for(Map.Entry<Integer,Integer> entry : hm.entrySet()){
+                  al.add(entry.getValue())
+              }
+          }
+    */
+    public static void main(String[] args) {
+        Map<Integer,Integer> hm = new TreeMap<>();
+        for(Map.Entry<Integer,Integer> entry : hm.entrySet()){
+            int key = entry.getKey();
+            int val = entry.getValue();
+        }
+    }
+}
