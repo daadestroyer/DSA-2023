bd3fcb266bb5211e2b789a88e48f7aa706174342
Shubham Nigam <48306820+daadestroyer@users.noreply.github.com>
2024-01-16 23:17:50 +0530

    right side view of bt completed...

---
+++ src/main/java/com/Trees/App14_RightViewOfBT.java
@@ -0,0 +1,21 @@
+package com.Trees;
+
+public class App14_RightViewOfBT {
+
+    /*
+        public void rightSideView(Node root,List<Integer> al,int level){
+            if(root == null)
+                return;
+
+            if(al.size() == level){
+                al.add(root.val);
+            }
+            rightSideView(root.right,al,level+1);
+            rightSideView(root.left,al,level+1);
+        }
+    */
+    public static void main(String[] args) {
+        // List<Integer> al = new ArrayList<>();
+        // rightSideView(root,al,0);
+    }
+}
