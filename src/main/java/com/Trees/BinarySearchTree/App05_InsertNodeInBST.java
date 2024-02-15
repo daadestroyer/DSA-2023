32160af1dd831ede9e083e7fb388b58bbbf92c98
Shubham Nigam <48306820+daadestroyer@users.noreply.github.com>
2024-02-15 23:03:41 +0530

    Insert node in bst added...

---
+++ src/main/java/com/Trees/BinarySearchTree/App05_InsertNodeInBST.java
@@ -0,0 +1,37 @@
+package com.Trees.BinarySearchTree;
+
+public class App05_InsertNodeInBST {
+    public static void main(String[] args) {
+        /*
+            public TreeNode insertIntoBST(TreeNode root, int val) {
+                if(root == null){
+                    return TreeNode(root);
+                }
+                TreeNode curr = root;
+
+                while(curr != null){
+                    if(curr.val <= val){
+                        // insert into right
+                        if(curr.right != null){
+                            curr = curr.right;
+                        }
+                        else{
+                            curr.right = new TreeNode(val);
+                            break;
+                        }
+                    }
+                    else{
+                        // insert into left
+                        if(curr.left != null){
+                            curr = curr.left;
+                        }
+                        else{
+                            curr.left = new TreeNode(val);
+                            break;
+                        }
+                    }
+                }
+            }
+        */
+    }
+}
