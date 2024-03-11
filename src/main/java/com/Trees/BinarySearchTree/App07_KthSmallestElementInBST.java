d2ae83bf42a424777d46caca50feb1b0e6084c15
Shubham Nigam <48306820+daadestroyer@users.noreply.github.com>
2024-03-11 23:27:32 +0530

    code added...

---
+++ src/main/java/com/Trees/BinarySearchTree/App07_KthSmallestElementInBST.java
@@ -0,0 +1,33 @@
+package com.Trees.BinarySearchTree;
+
+public class App07_KthSmallestElementInBST {
+
+    int count = 0;
+    int res = Integer.MIN_VALUE;
+
+    /*
+        public void inorderTraversal(Node root , int k){
+            if(root == null){
+                return;
+            }
+            // left
+            inorderTraversal(root.left,k);
+
+            // root
+            count++;
+            if(count == k){
+                res = root.val;
+                return res;
+            }
+
+            // right
+            inorderTraversal(root.right,k);
+        }
+    */
+    public static void main(String[] args) {
+        /*
+            inorderTraversal(root,k);
+            return res;
+        */
+    }
+}
--- src/main/java/com/Trees/BinaryTree/App02_InOrderTraversal_BT.java
+++ src/main/java/com/Trees/BinaryTree/App02_InOrderTraversal_BT.java
@@ -5,34 +5,35 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.Queue;
 
+class TreeNode{
+    int data;
+    Node left;
+    Node right;
+    public TreeNode(int data){
+        this.data = data;
+    }
+}
 public class App02_InOrderTraversal_BT {
 
-/*
-    public List<List<Integer>> levelOrderTraversal(Node root){
-        Queue<Integer> q = new LinkedList<>();
-        List<List<Integer> list = new ArrayList<>();
 
+    public static void InOrderTraversal(Node root,List<Integer> list) {
         if(root == null)
-            return list;
+            return ;
 
-        q.add(root);
-        while(!q.isEmpty()){
-            int size = q.size();
-            for(int i=0 ; i<size ; i++){
-                if(q.peek().left != null)
-                    q.add(q.peek().left)
-                if(q.peek().right != null)
-                    q.add(q.peek().right)
 
-                subList.add(q.remove().val)
-            }
-        }
-        return list;
+        InOrderTraversal(root.left,list);
+        list.add(root.data);
+        InOrderTraversal(root.right,list);
+
     }
 
-*/
     public static void main(String[] args) {
-        List<List<Integer>> al = new ArrayList<>();
-        Queue<Integer> q = new LinkedList<>();
+        List<Integer> list = new ArrayList<>();
+        InOrderTraversal(new Node(10),list);
+        InOrderTraversal(new Node(11),list);
+        InOrderTraversal(new Node(13),list);
+        InOrderTraversal(new Node(3),list);
+        InOrderTraversal(new Node(4),list);
+        System.out.println(list);
     }
 }
